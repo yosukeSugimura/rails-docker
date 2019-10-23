226844e49b705c744e57b1264ebf71c0e50b5f39
Yosuke Sugimura <yosuke.sugimura@genexsolutions.co.jp>
2019-10-23 12:17:18 +0900

    gemの更新

--- app/models/result_string_count.rb
+++ app/models/result_string_count.rb
@@ -1,10 +1,10 @@
 class ResultStringCount < ApplicationRecord
-    belongs_to :rank_master
+    belongs_to :rank_master, optional: true
     alias_attribute :rank, :rank_master
 
-    belongs_to :result_comment_master
+    belongs_to :result_comment_master, optional: true
     alias_attribute :comment, :result_comment_master
 
-    belongs_to :result_detaile_master, :foreign_key => "id"
+    belongs_to :result_detaile_master, :foreign_key => "id", optional: true
     alias_attribute :detaile, :result_detaile_master
 end
